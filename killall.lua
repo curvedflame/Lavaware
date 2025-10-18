8566cfed18e908d5ef5624bbdbb8e6d6ba6211a9
curvedflame <209521000+curvedflame@users.noreply.github.com>
2025-10-18 12:01:47 +0200

    Update killall.lua

--- killall.lua
+++ killall.lua
@@ -1,6 +1,5 @@
 local StarterGui = game:GetService("StarterGui")
 
--- Send notification
 StarterGui:SetCore("SendNotification", {
 	Title = "script temp down";
 	Text = "script removed temp until i can do sum stuff";
